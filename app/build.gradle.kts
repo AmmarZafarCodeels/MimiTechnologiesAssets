edd362e37a7cc8343f1c6184dc67423b731d8743
apple <ammar@codeels.com>
2025-05-30 12:12:26 -0700

    Init Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.application") version "8.10.1"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+}
+
+android {
+    namespace = "com.ammar.appassetsupload"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ammar.appassetsupload"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    // buildFeatures { compose = false } // Not needed, default is false
+}
+
+dependencies {
+    // Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+
+    // Jetpack Lifecycle, ViewModel, LiveData (MVVM essentials)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.common.java8)
+
+    // Activity & Fragment KTX
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
---
+++ app/src/main/java/com/ammar/appassetsupload/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ammar.appassetsupload
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
